02caead4cc4411eef3d8b192fe6255a7db535d6d
qcypggs <qcypggs@qq.com>
2023-04-29 01:07:00 +0800

    xiugairun.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -dit --name=simplegpt4 -p 44:44 simplegpt4
+docker run -dit --name=simplegpt4 -p 44:44 -e OPENAI_API_KEY=sk-xxxxxxxxxxxxxx -e OPENAI_API_BASE=https://api.openai.com simplegpt4
